ab72c186ebe76e18b60f31fa47376ca6e5dd42c8
pham259 <jennifer.pham@pnnl.gov>
2025-02-26 20:51:32 -0800

    adjust box layouts

--- pymoods-react/src/App.tsx
+++ pymoods-react/src/App.tsx
@@ -8,14 +8,12 @@ import './App.css'
 function App() {
 
   return (
-      <Box sx={{ display: 'flex' }}>
-        <SideMenu />
-      <Box sx={{ flexGrow: 1 }}>
-        <AppNavbar />
-        {/* Main content */}
-        <Box component="main" sx={{ padding: '16px' }}>
-          <MainGrid />
-        </Box>
+    <Box sx={{ display: 'flex' }}>
+      <SideMenu />
+      <AppNavbar />
+      {/* Main content */}
+      <Box component="main" sx={{ padding: '16px' }}>
+        <MainGrid />
       </Box>
     </Box>
   );
